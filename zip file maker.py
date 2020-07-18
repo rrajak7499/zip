0f5f0d46ec35eedb8fc3b7dd3d9a4f3648514cab
rrajak7499 <56112857+rrajak7499@users.noreply.github.com>
2020-07-18 13:10:55 +0530

    Create zip file maker.py

---
+++ zip file maker.py
@@ -0,0 +1,9 @@
+#***** Step : 1 ***** 
+from google.colab 
+import drive drive.mount('/content/drive')
+
+#***** Step : 2 ***** 
+cd '/content/drive/My Drive/Torrent/file path'
+
+#***** Step : 3 ***** 
+!zip -r ../file_name.zip .
